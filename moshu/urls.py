e8d5d343c20d756fd2ce2a075346df0dfe20dba5
AlpacaRex <1030519668@qq.com>
2022-08-04 21:10:58 +0800

    edit upload download

--- moshu/urls.py
+++ moshu/urls.py
@@ -1,4 +1,4 @@
-"""墨书 URL Configuration
+"""moshu URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
